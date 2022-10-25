dcb1edeebd32140c41637804c4235df2bfc4431d
jshahadat <jshahadat8@gmail.com>
2022-10-26 02:31:57 +0600

    topic data done

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const category = require('./data/category.json')
+const topic = require('./data/topic.json');
+
 
 
 app.get('/', (req, res) => {
@@ -16,6 +18,27 @@ app.get('/', (req, res) => {
 app.get('/coures-category', (req, res) => {
     res.send(category)
 })
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '08') {
+        res.send(topic);
+    }
+    else {
+        const category_topic = topic.filter(n => n.category_id === id);
+        res.send(category_topic);
+    }
+})
+
+app.get('/topic', (req, res) => {
+    res.send(topic);
+});
+
+app.get('/topic/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedTopic = topic.find(n => n._id === id);
+    res.send(selectedTopic);
+});
 
 app.listen(port, () => {
     console.log('Assignment ten Server running on port', port);
